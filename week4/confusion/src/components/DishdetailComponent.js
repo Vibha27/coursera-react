53dbca5bd2bb4ebf41498f970be76d67864ddc4a
vibha singh <svibha081@gmail.com>
2020-05-12 00:21:02 +0530

    react-animation-component

--- week4/confusion/src/components/DishdetailComponent.js
+++ week4/confusion/src/components/DishdetailComponent.js
@@ -4,19 +4,23 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors  } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components'
 
     function RenderDish({dish}) {
         if(dish != null) {
             return (
-                
-                <Card>
-                    <CardImg  src={baseUrl + dish.image}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-                
+                <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)' 
+                }}>
+                    <Card>
+                        <CardImg  src={baseUrl + dish.image}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             );
         }else{
             return (
@@ -30,20 +34,26 @@ import { baseUrl } from '../shared/baseUrl';
         return (
             <div>
                 <h4>Comments</h4>
-                {comments.map((comment)=>{
 
-                    return (
+                <Stagger in>
+                    {comments.map((comment)=>{
 
-                        <div key={comment.id}>   
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US',{ year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                               
+                        return (
+                            <Fade in>
+                                <div key={comment.id}>   
+                                            <p>{comment.comment}</p>
+                                            <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US',{ year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    
+                                
+                                </div>
+                            </Fade>
+                        );
                         
-                        </div>
-                    );
-                    })
+                        })
+                        
+                    }
+                </Stagger>
 
-                }
                 {/* CommentForm Component */}
                 <CommentForm dishId={dishId} postComment={postComment} />
 
--- week4/confusion/src/components/MainComponent.js
+++ week4/confusion/src/components/MainComponent.js
@@ -10,7 +10,7 @@ import { Switch,Route,Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-
+import { TransitionGroup, CSSTransition} from 'react-transition-group';
 const mapStoreToProps = state => {
   return {
     dishes : state.dishes,
@@ -77,16 +77,20 @@ class Main extends Component {
       <div className="App">
         
         <Header />
-        <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />}/>
-            <Route path="/menu/:dishId" component={DishWithId} />}/>
-            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
-            <Route path="/aboutus" component={()=> <About leaders={this.props.leaders} />} />
-            
-      {/* if any of path not matches then redirect to default i.e '/home' */}
-            <Redirect to="/home" />
-        </Switch>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+            <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />}/>
+                <Route path="/menu/:dishId" component={DishWithId} />}/>
+                <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
+                <Route path="/aboutus" component={()=> <About leaders={this.props.leaders} />} />
+                
+          {/* if any of path not matches then redirect to default i.e '/home' */}
+                <Redirect to="/home" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
         </div>
     );
